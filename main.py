19a9f1accce0a609eeee2ca64d5430da067e9da0
Timothy Graham <59664459+sliceanddiceman@users.noreply.github.com>
2021-01-18 20:33:00 -0600

    Update main.py
    Just a simple login program

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 # just a basic login in program.
 
 sname = 1234
-spasword = 1234
+spassword = 1234
 name = input("enter your login name: ")
 password = input("enter you password: ")
 
 
-def add_me(name, password, sname, spasword):
-    if int(name) == int(sname) and int(password) == int(spasword):
+def add_me(name, password, sname, spassword):
+    if int(name) == int(sname) and int(password) == int(spassword):
         print("user name correct")
         print("password correct, you are logged in.")
     else:
         print("Login failed")
 
 
-add_me(name, password, sname, spasword)
+add_me(name, password, sname, spassword)
